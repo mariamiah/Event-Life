0e0f37e13de6936370e56502999520dec824f34e
= <=>
2018-09-04 16:12:57 +0300

    Modify reception.py file

--- reception/reception.py
+++ reception/reception.py
@@ -1,7 +1,3 @@
-# Declare empty lists that shall contain names read from the files
-ordinary_list = []
-vip_list = []
-
 # Read ordinary file and append names to a list
 ordinary = open("ordinary.txt", "r")
 ordinary_list = [line.rstrip() for line in ordinary.readlines()]
@@ -15,28 +11,20 @@ vip_list = [line.rstrip() for line in vip.readlines()]
 singlename = input("Enter single name:")
 
 
-def registration_checker_ordinary(singlename):
-    ''' Function to check if a guest is registered
-     in ordinary list '''
-    return ''.join(name for name in ordinary_list if singlename in name) + ", ORDINARY"
-
-
-def registration_checker_vip(singlename):
-    ''' Function to check if a guest is registered
-    in vip_list'''
-    return ''.join(name for name in vip_list if singlename in name) + ", VIP"
-
-
 def registration_checker(singlename):
     ''' Function that checks if the user exists either in ordinary_list
     or in vip_list'''
-    if singlename in ordinary_list:
-        return registration_checker_ordinary(singlename)
-    elif singlename in vip_list:
-        return registration_checker_vip(singlename)
-    else:
-        return "User not registered"
-        
+    for name in ordinary_list:
+        if name.split()[0] == singlename:
+            return [name, "ORDINARY"]
+
+    for name in vip_list:
+        if name.split()[0] == singlename:
+            return [name, "VIP"]
+    
+    for name in ordinary_list and vip_list:
+        if singlename not in name.split()[0]:
+            return "User does not exist"
 print(registration_checker(singlename))
 
 ordinary.close()
