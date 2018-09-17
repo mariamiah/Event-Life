360c405fafbdbbc4c957ab10438d97b68f792b0a
= <=>
2018-09-17 14:46:14 +0300

    Modify reception file [Delivers #160203221]

--- reception/reception.py
+++ reception/reception.py
@@ -15,15 +15,15 @@ def registration_checker(singlename):
     ''' Function that checks if the user exists either in ordinary_list
     or in vip_list'''
     for name in ordinary_list:
-        if name.split()[0].lower() == singlename.lower():
+        if name.split()[0].lower() == singlename.lower() or name.split()[1].lower() == singlename.lower():
             return [name, "ORDINARY"]
 
     for name in vip_list:
-        if name.split()[0].lower() == singlename.lower():
+        if name.split()[0].lower() == singlename.lower() or name.split()[1].lower() == singlename.lower():
             return [name, "VIP"]
     
     for name in ordinary_list and vip_list:
-        if singlename.lower() not in name.split()[0].lower():
+        if singlename.lower() not in name.split()[0].lower() or singlename.lower() not in name.split()[1].lower():
             return "User does not exist"
 print(registration_checker(singlename))
 
